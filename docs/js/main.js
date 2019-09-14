4019e7eaa983816426e28f620803adc38dbc3ce0
Kirill Genets <kirill.genets2002@mail.ru>
2019-09-14 17:59:19 +0300

    fixed some shit

--- docs/js/main.js
+++ docs/js/main.js
@@ -429,7 +429,7 @@ function onClientsSliderInitialized(evt) {
 	} else if (document.documentElement.clientWidth <= 800 && document.documentElement.clientWidth >= 520) {
 		clientsCounterAmount.text(evt.item.count - 1);
 	} else if (document.documentElement.clientWidth <= 520) {
-		clientsCounterAmount.text(evt.item.count + 1);
+		clientsCounterAmount.text(evt.item.count);
 	}	else {
 		clientsCounterAmount.text(evt.item.count - 5);
 	}
